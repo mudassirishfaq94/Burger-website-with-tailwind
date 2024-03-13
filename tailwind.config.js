8af396ecccd99c3a398b685bd2e7bbeef0768738
mudassirishfaq94 <mudassir.dev09@gmail.com>
2024-03-13 05:58:17 -0700

    index html updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor:'#010a5e',
+        primarycolorLight:'#010d78',
+        secondaryColor:'#FFCC00',
+        paragrapghColor:'#c0c0c0',
+        whiteColor: '#fff',
+        blackColor:'#000',
+        greenColor:'#007936',
+        redColor: '#cc3433',
+        darkcolorLight:'#171717',
+      }
+      
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1.5rem',
+      }
+    }
   },
   plugins: [],
 }
